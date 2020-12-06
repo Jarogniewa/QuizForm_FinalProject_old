ddf57c7aab949d1ea6b07c4158395215b0230297
Jarogniewa <marta.jeczen@o2.pl>
2020-12-06 09:51:15 +0100

    Second Commit

---
+++ src/main/java/pl/sda/QuizForm_FinalProject/QuizForm_FinalProject.java
@@ -0,0 +1,13 @@
+package pl.sda.QuizForm_FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizForm_FinalProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizForm_FinalProject.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/QuizForm_FinalProject/QuizForm_FinalProjectTests.java
@@ -0,0 +1,13 @@
+package pl.sda.QuizForm_FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizForm_FinalProjectTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
